b0ea6709595cc586ef0b1e5548b83f9c1c733dfb
21 21 <48380712+Pelshemka@users.noreply.github.com>
2023-11-24 10:43:50 +0200

    Add files via upload

--- 3.7/script.js
+++ 3.7/script.js
@@ -7,4 +7,20 @@ $(document).ready(function(){
 
     // Вибираємо всі елементи <li> та додаємо їм клас list-item
     $('li').addClass('list-item');
+	
+	// Вибираємо всі елементи з класом highlight та змінюємо їх колір на червоний
+	$('.highlight').css('color', '#FF0000'); 
+
+	// Вибираємо всі елементи з класом list-item та змінюємо їх фон на світло-сірий
+	$('.list-item').css('background-color', '#D3D3D3'); 
+
+	// Вибираємо всі елементи з класом highlight та застосовуємо до них ефект fadeIn
+	$('.highlight').hide().fadeIn('slow');
+	
+	$('#task1 p:last').text("Новий останній текст");
+
+	$('#additional_task').click(function(){
+    $('.list-item').remove();
+});
+	
 });
